aa2ae05726daaf0c8a16a224890f74d9a4dd85f7
alexgordontlv <alexaybn@gmail.com>
2021-03-04 15:02:55 +0200

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const PORT = process.env.PORT || 5000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("index", { roomId: req.params.roomId });
+});
+
+server.listen(PORT, () => {
+  console.log("serving on port:", PORT);
+});
