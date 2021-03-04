b9ca8928e7927e38bf3b5c1213befae08dc84f2d
alexgordontlv <alexaybn@gmail.com>
2021-03-04 15:18:22 +0200

    hi

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log("hi");
+  console.log("hi ");
   res.redirect(`/${uuidv4()}`);
 });
 
